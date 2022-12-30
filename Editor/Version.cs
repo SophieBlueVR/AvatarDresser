c0f8c08a1e53364048e5489cc2e26364a10847ac
sophie <sophie@somethingsomething.org>
2022-12-29 22:16:50 -0500

    fix version file too

--- Editor/Version.cs
+++ Editor/Version.cs
@@ -4,6 +4,6 @@ namespace SophieBlue.AvatarDresser {
     [UsedImplicitly]
     internal static class Version
     {
-        [UsedImplicitly] public const string VERSION = "v0.1.2";
+        [UsedImplicitly] public const string VERSION = "v0.1.5";
     }
 }
